f9a70f4381e7a754ebd109e9406dabbe9e173ade
Jordan Jordanov <d_yugioh@abv.bg>
2020-04-17 07:52:14 +0300

    v1.0.4

---
+++ backend/src/ProductCatalog.Api/Configuration/DependenciesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Api.Domain.Repositories;
+using ProductCatalog.Api.Persistence;
+
+namespace ProductCatalog.Api.Configuration
+{
+    public static class DependenciesConfiguration
+    {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductsRepository, ProductsRepository>();
+            return services;
+        }
+    }
+}
