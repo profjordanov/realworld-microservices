e6227ce6b7ab517bae793271d85b4d679e16240c
Jordan Jordanov <d_yugioh@abv.bg>
2020-05-05 07:17:57 +0300

    v1.3.3

---
+++ frontend/web.manager/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Stocks } from "./Stocks";
+
+function App() {
+    return (
+        <div className="App">
+            <Stocks />
+        </div>
+    );
+}
+
+export default App;
